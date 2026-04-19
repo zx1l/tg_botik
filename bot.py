a992d7fb1c8239f66bc7b1da62eb8e4f6f5c2e5e
zx1l <zaezzaez@gmail.com>
2026-04-19 18:15:25 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+import telebot as tb
+from google import genai
+
+key = 
+bot = tb.TeleBot(key)
+
+GEMINI_API_KEY = 
+client = genai.Client(api_key = GEMINI_API_KEY)
+
+
+
+@bot.message_handler(commands = ['start'])
+def start_message(message):
+  bot.send_message(message.chat.id, 'Введите свой запрос и Gemini-3-flash ответит вам')
+
+def gemini(message):
+  print(message.text)
+  response = client.models.generate_content(
+      model="gemini-3-flash-preview", contents=message.text
+  )
+  print(response.text)
+  bot.send_message(message.chat.id, response.text)
+  bot.register_next_step_handler(message, gemini)
+
+@bot.message_handler(content_types = ['text'])
+def zapros(message):
+  bot.register_next_step_handler(message, gemini)
+
+
+bot.infinity_polling()
+
